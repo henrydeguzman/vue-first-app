c3db95bdea77a041374b78e54286940f292779fb
henrilics <henrydeguzman.java73@gmail.com>
2020-11-04 16:25:47 +0800

    updates

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,37 +1,47 @@
 <template>
-  <li>
-    <h2>{{ friend.name }}</h2>
-    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
-    <ul v-if="detailsAreVisible">
-      <li>
-        <strong>Phone:</strong>
-        {{ friend.phone }}
-      </li>
-      <li>
-        <strong>Email:</strong>
-        {{ friend.email }}
-      </li>
-    </ul>
-  </li>
+	<li>
+		<h2>{{ name }}</h2>
+		<button @click="toggleDetails">
+			{{ detailsAreVisible ? "Hide" : "Show" }} Details
+		</button>
+		<ul v-if="detailsAreVisible">
+			<li>
+				<strong>Phone:</strong>
+				{{ phoneNumber }}
+			</li>
+			<li>
+				<strong>Email:</strong>
+				{{ emailAddress }}
+			</li>
+		</ul>
+	</li>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      detailsAreVisible: false,
-      friend: {
-        id: "manuel",
-        name: "Manuel Lorenz",
-        phone: "0123 45678 90",
-        email: "manuel@localhost.com",
-      },
-    };
-  },
-  methods: {
-    toggleDetails() {
-      this.detailsAreVisible = !this.detailsAreVisible;
-    }
-  }
+     // props: [
+     //      'name',
+     //      'phoneNumber',
+     //      'emailAddress'
+     // ],
+     props: {
+          name: {
+               type: String,
+               required: true
+          },
+          phoneNumber: String,
+          emailAddress: String,
+          isFavorite: String
+     },
+	data() {
+		return {
+			detailsAreVisible: false
+		};
+	},
+	methods: {
+		toggleDetails() {
+			this.detailsAreVisible = !this.detailsAreVisible;
+		},
+	},
 };
 </script>
